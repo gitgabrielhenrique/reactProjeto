9fcf7d4ed55a508b4bf9d0202092f823f0aed0df
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-08-09 08:55:04 -0300

    update

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,8 +1,17 @@
+
+import Logo from '../../assets/logo.svg'
+import styles from './Cardapio.module.scss'
 export default function Cardapio(){
 
     return(
         <div>
-                    Cardapio do restaurante
+<main>
+  <nav className={styles.menu}>
+<img src={Logo} alt="Logo restaurante" />
+
+  </nav>
+</main>
+
         </div>
     );
 }
