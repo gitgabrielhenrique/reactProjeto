c0df631005202a0406765e7d149a5fdbae514bba
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-08-23 08:55:57 -0300

    update

--- restaurante/src/pages/Cardapio/index.tsx
+++ restaurante/src/pages/Cardapio/index.tsx
@@ -1,17 +1,27 @@
 
-import Logo from '../../assets/logo.svg'
+import Logo from 'assets/logo.svg'
 import styles from './Cardapio.module.scss'
 export default function Cardapio(){
 
     return(
-        <div>
+
 <main>
   <nav className={styles.menu}>
 <img src={Logo} alt="Logo restaurante" />
 
   </nav>
+<header className={styles.header}>
+<div className={styles.header__text}>
+  "a casa do codigo e da massa"
+</div>
+</header>
+<section>
+  <h3>cardapio</h3>
+</section>
+
+
 </main>
 
-        </div>
+
     );
 }
